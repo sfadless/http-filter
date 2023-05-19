9859faa1379a198aa614391093cc937c7719512f
sfadless <pgolikov327@gmail.com>
2023-05-19 20:49:18 +0300

    add more filters to adapter

--- src/Adapter/DoctrineQueryBuilderAdapter.php
+++ src/Adapter/DoctrineQueryBuilderAdapter.php
@@ -16,6 +16,17 @@ use Sfadless\HttpFilter\HttpFilterInterface;
  */
 final class DoctrineQueryBuilderAdapter extends AbstractAdapter
 {
+    /**
+     * @param array $fieldsMapping массив для сопоставления поля в QueryBuilder и названия этого поля в фильтре
+     * @example
+     * $qb
+     *     ->select("client")
+     *     ->from(App::Client, "client")
+     * ;
+     * $fieldsMapping = [
+     *     "name" => "client.name"
+     * ]
+     */
     public function __construct(private readonly QueryBuilder $qb, private readonly array $fieldsMapping = []){}
 
     public function applyFilter(HttpFilterInterface $httpFilter): void
@@ -41,6 +52,10 @@ final class DoctrineQueryBuilderAdapter extends AbstractAdapter
         switch ($operator) {
             case FilterFieldOperator::EQUAL: return $expr->eq($fieldName, $expr->literal("$fieldValue"));
             case FilterFieldOperator::LIKE: return $expr->like($fieldName, $expr->literal("%$fieldValue%"));
+            case FilterFieldOperator::GREATER: return $expr->gt($fieldName, $expr->literal("$fieldValue"));
+            case FilterFieldOperator::GREATER_OR_EQUAL: return $expr->gte($fieldName, $expr->literal("$fieldValue"));
+            case FilterFieldOperator::LOWER: return $expr->lt($fieldName, $expr->literal("$fieldValue"));
+            case FilterFieldOperator::LOWER_OR_EQUAL: return $expr->lte($fieldName, $expr->literal("$fieldValue"));
         }
 
         throw new Exception("Not implementer logic for $operator->value");
